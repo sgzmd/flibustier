815a467f3f5cab6d4b988d23ff7a40b16bf5000b
Roman Kirillov <sigizmund@gmail.com>
2019-12-20 19:21:06 +0000

    Can now take Flibusta URLs

--- flibustier-web/src/main/java/com/sgzmd/flibustier/backend/SequenceService.kt
+++ flibustier-web/src/main/java/com/sgzmd/flibustier/backend/SequenceService.kt
@@ -18,6 +18,7 @@ class SequenceService {
     fun fetch(query: String, offset: Int, limit: Int) : Stream<Series> {
         logger.info("Fetching for q=$query with offset=$offset, limit=$limit")
 
+        var q = rewriteQuery(query)
         var rs: ResultSet? = null
 
         val statement = connection.createStatement()
@@ -25,7 +26,7 @@ class SequenceService {
             rs = statement.executeQuery("select SeqName, Authors, SeqId from sequence_fts LIMIT $limit OFFSET $offset")
         } else {
             rs = statement.executeQuery("select SeqName, Authors, SeqId from sequence_fts " +
-                    "where sequence_fts match '$query' LIMIT $limit OFFSET $offset")
+                    "where sequence_fts match '$q' LIMIT $limit OFFSET $offset")
         }
 
         val sequences = mutableListOf<Series>()
@@ -39,6 +40,16 @@ class SequenceService {
         return sequences.stream()
     }
 
+    private fun rewriteQuery(query: String): String? {
+        val re = Regex(".+sequence\\/([0-9]+)")
+        var q: String? = query
+        if (re.matches(query)) {
+            val group = re.matchEntire(query)?.groups?.get(1)
+            q = group?.value
+        }
+        return q
+    }
+
 
     fun numEntries(query: String) : Int {
         var rs: ResultSet? = null
@@ -46,8 +57,9 @@ class SequenceService {
         if (query == "") {
             rs = statement.executeQuery("select COUNT(1) as Cnt from sequence_fts")
         } else {
+            var q = rewriteQuery(query)
             rs = statement.executeQuery("select COUNT(1) as Cnt from sequence_fts " +
-                    "where sequence_fts match '$query'")
+                    "where sequence_fts match '$q'")
         }
 
         if (rs.next()) {
