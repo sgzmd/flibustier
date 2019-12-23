b29db4a8febc0d09b1f65ba7b2c9dfffe2e31c3d
Roman Kirillov <sigizmund@gmail.com>
2019-12-23 20:33:10 +0000

    .

--- flibustier-web/src/main/java/com/sgzmd/flibustier/MainView.java
+++ flibustier-web/src/main/java/com/sgzmd/flibustier/MainView.java
@@ -3,6 +3,7 @@ package com.sgzmd.flibustier;
 import com.okta.spring.boot.oauth.Okta;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
