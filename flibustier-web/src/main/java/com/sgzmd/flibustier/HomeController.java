36f96e0583da8a3dca8da68c4732efa2c6269b0e
Roman Kirillov <sigizmund@gmail.com>
2019-12-23 14:11:25 +0000

    Sign-in with GitHub is working now.

--- flibustier-web/src/main/java/com/sgzmd/flibustier/HomeController.java
+++ flibustier-web/src/main/java/com/sgzmd/flibustier/HomeController.java
@@ -3,7 +3,10 @@ package com.sgzmd.flibustier;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,17 +20,28 @@ public class HomeController {
 
     ObjectMapper mapper = new ObjectMapper();
 
-    @GetMapping("/securedPage")
-    public ModelAndView home(@AuthenticationPrincipal OidcUser user) throws JsonProcessingException {
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("user", user.getUserInfo().getEmail());
-        mav.addObject("idToken", user.getIdToken().getTokenValue());
-        mav.addObject(
-                "claims",
-                mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user.getClaims())
-        );
-        mav.setViewName("securedPage");
-        return mav;
+//    @GetMapping("/securedPage")
+//    public ModelAndView home(@AuthenticationPrincipal OidcUser user) throws JsonProcessingException {
+//        ModelAndView mav = new ModelAndView();
+//        mav.addObject("user", user.getUserInfo().getEmail());
+//        mav.addObject("idToken", user.getIdToken().getTokenValue());
+//        mav.addObject(
+//                "claims",
+//                mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user.getClaims())
+//        );
+//        mav.setViewName("securedPage");
+//        return mav;
+//    }
+
+
+    @RequestMapping("/securedPage")
+    public String securedPage(Model model,
+                              @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                              @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("userName", oauth2User.getName());
+        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+        model.addAttribute("userAttributes", oauth2User.getAttributes());
+        return "securedPage";
     }
 
     @RequestMapping("/")
