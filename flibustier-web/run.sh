99de5c64e915564a9bbdf80f28174284365cd22b
Roman Kirillov <sigizmund@gmail.com>
2019-12-25 10:47:11 +0000

    Removing old flibustier-web

--- flibustier-web/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash -x
-
-mvn package && java -jar target/flibustier-2.0-SNAPSHOT.jar --spring.config.location=../config/authconfig.yml,classpath:/application.yml
--- flibustier-web/src/main/java/com/sgzmd/flibustier/backend/Series.kt
+++
@@ -1,7 +0,0 @@
-package com.sgzmd.flibustier.backend
-
-data class Series(val sequenceName: String, val authors: String, val seqId: Int) {
-    override fun toString(): String {
-        return "$sequenceName - $authors"
-    }
-}
