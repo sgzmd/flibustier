9d9f5b9c7904b494bb3d1db8478efd1d133a86d0
Roman Kirillov <sigizmund@gmail.com>
2019-12-27 10:15:37 +0000

    Adding proper constructor for UpdateChecker

--- web/src/main/kotlin/com/sgzmd/flibustier/web/scheduled/UpdateChecker.kt
+++ web/src/main/kotlin/com/sgzmd/flibustier/web/scheduled/UpdateChecker.kt
@@ -1,9 +1,14 @@
 package com.sgzmd.flibustier.web.scheduled
 
+import com.sgzmd.flibustier.web.db.IEntryUpdateStatusProvider
+import com.sgzmd.flibustier.web.db.SqlLiteEntryUpdateStatusProvider
 import com.sgzmd.flibustier.web.db.TrackedEntryRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class UpdateChecker(@Autowired val trackedEntryRepository: TrackedEntryRepository) {
+class UpdateChecker(
+    @Autowired val trackedEntryRepository: TrackedEntryRepository,
+    @Autowired val entryUpdateStatusProvider: IEntryUpdateStatusProvider) {
+
 }
