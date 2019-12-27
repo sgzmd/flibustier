616ec2cae0b8f6eeeff8761d177d1703c4c90642
Roman Kirillov <sigizmund@gmail.com>
2019-12-27 09:49:14 +0000

    IndexController is now properly tested using MockMvc.

--- web/src/test/kotlin/com/sgzmd/flibustier/web/controllers/IndexControllerTest.kt
+++ web/src/test/kotlin/com/sgzmd/flibustier/web/controllers/IndexControllerTest.kt
@@ -1,44 +1,50 @@
 package com.sgzmd.flibustier.web.controllers
 
+import com.sgzmd.flibustier.web.db.FoundEntryType
 import com.sgzmd.flibustier.web.db.GlobalSearch
-import com.sgzmd.flibustier.web.db.IGlobalSearch
-import com.sgzmd.flibustier.web.db.TrackedEntryRepository
-import com.sgzmd.flibustier.web.security.AuthenticationFacade
+import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.stereotype.Component
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig
 import org.springframework.test.context.junit4.SpringRunner
-import java.lang.RuntimeException
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
 @SpringBootTest
 @RunWith(SpringRunner::class)
 @ActiveProfiles("test")
-@ContextConfiguration
+@SpringJUnitWebConfig
 class IndexControllerTest() {
-  @Autowired lateinit var repo: TrackedEntryRepository
-  @Autowired lateinit var authFacade: AuthenticationFacade
+  @Autowired lateinit var wac: WebApplicationContext
 
-  lateinit var indexController: IndexController
+  lateinit var mockMvc: MockMvc
 
   @Before
   fun setUp() {
-    indexController = IndexController(FakeGlobalSearch(), repo, authFacade)
-  }
-
-  @Component
-  class FakeGlobalSearch : IGlobalSearch {
-    override fun search(searchTerm: String): List<GlobalSearch.SearchResult> {
-      return mutableListOf()
-    }
+    mockMvc = MockMvcBuilders.webAppContextSetup(wac).build()
   }
 
   @Test
+  @WithMockUser("testuser")
   fun testGlobalSearch() {
-    indexController.globalSearch.search("abc")
+    mockMvc.get("/?search_term=Унес").andExpect {
+      status { isOk }
+      model {
+        attribute("searchResults",
+            Matchers.equalTo(arrayListOf(
+                GlobalSearch.SearchResult(
+                    FoundEntryType.SERIES,
+                    name = "Унесенный ветром",
+                    entryId = 34145,
+                    numEntities = 10))))
+      }
+    }
   }
 }
