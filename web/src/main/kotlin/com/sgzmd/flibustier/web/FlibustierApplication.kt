d5e72132d9dde8990efb21b23a1f57dad3c5b2df
Roman Kirillov <sigizmund@gmail.com>
2020-01-15 09:31:57 +0000

    More logging changes

--- web/src/main/kotlin/com/sgzmd/flibustier/web/FlibustierApplication.kt
+++ web/src/main/kotlin/com/sgzmd/flibustier/web/FlibustierApplication.kt
@@ -1,11 +1,15 @@
 package com.sgzmd.flibustier.web
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class FlibustierApplication
 
+val auditLog = LoggerFactory.getLogger("audit")
+
 fun main(args: Array<String>) {
+	auditLog.info("Starting Flibustier...")
 	runApplication<FlibustierApplication>(*args)
 }
