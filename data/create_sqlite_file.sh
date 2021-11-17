bdaffbe5920dd5627b7d1eed675a641710361f37
Roman Kirillov <sigizmund@gmail.com>
2021-11-17 18:57:34 +0000

    Allowing send Telegram updates

--- data/create_sqlite_file.sh
+++ data/create_sqlite_file.sh
@@ -8,16 +8,62 @@ function assert_exists {
   fi
 }
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ./create_sqlite_file.sh <active directory> </path/to/final/flibusta.db>"
-    exit 2
-fi
+function send_update {
+  key=$1
+  chat=$2
+  text=$3
 
-active_dir=$1
-flibusta_db_path=$2
+  if [ ! -z "$key" ] && [ ! -z "$chat" ] 
+  then
+  curl -X POST "https://api.telegram.org/bot$key/sendMessage" -d "chat_id=$chat&text=$text"
+  echo $text
+  else 
+  echo "No Telegram, trace: $text"
+  fi
+}
+
+# list of arguments expected in the input
+optstring="d:t:c:f:"
+
+active_dir="."
+telegram_key=""
+telegram_chat_id=""
+flibusta_db_path="none.db"
+
+while getopts ${optstring} arg; do
+  case ${arg} in
+    t) 
+      echo "Telegram key ${OPTARG}"
+      telegram_key="${OPTARG}"
+      ;;
+    c) 
+      echo "Telegram chat ID ${OPTARG}"
+      telegram_chat_id="${OPTARG}"
+      ;;
+    f) 
+      flibusta_db_path="${OPTARG}"
+      echo "Using flibusta db path $flibusta_db_path"
+      ;;
+    d) 
+      active_dir="${OPTARG}"
+      echo "Using active directory $active_dir"
+      ;;
+    :)
+      echo "$0: Must supply an argument to -$OPTARG." >&2
+      exit 1
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 2
+      ;;
+  esac
+done
+
+echo "Active dir: $active_dir, flibusta db: $flibusta_db_path, T-Key: $telegram_key, Chat ID: $telegram_chat_id"
 
 if [ ! -d $active_dir ]; then 
-  echo "Active directory $1 doesn't exist"
+  send_update $telegram_key $telegram_chat_id "Active directory $active_dir doesn't exist"
+  
   exit 1
 fi
 
@@ -35,8 +81,8 @@ assert_exists sqlite3
 ./main
 
 if [ $? -ne 0 ]; then
-    echo "File downloader has failed with error code $?"
-    exit 1
+    send_update $telegram_key $telegram_chat_id "File downloader has failed with error code $?"
+    exit 11
 fi
 
 # Unpacking files
@@ -48,7 +94,7 @@ cat lib*.sql > flibusta_mysql_dump.sql
 # Applying MySQL -> sqlite3 converter script
 awk -f mysql2sqlite flibusta_mysql_dump.sql > flibusta_sqlite_dump.sql
 if [ $? -ne 0 ]; then
-    echo "mysql2sqlite has failed with error code $?"
+    send_update $telegram_key $telegram_chat_id "mysql2sqlite has failed with error code $?"
     exit 2
 fi
 
@@ -59,7 +105,7 @@ rm -rf flibusta_new.db
 sqlite3 flibusta_new.db < flibusta_sqlite_dump.sql
 
 if [ $? -ne 0 ]; then
-    echo "sqlite3 db creation has failed with error code $?"
+    send_update $telegram_key $telegram_chat_id "sqlite3 db creation has failed with error code $?"
     exit 3
 fi
 
@@ -67,7 +113,7 @@ fi
 sqlite3 flibusta_new.db < SequenceAuthor.sql
 
 if [ $? -ne 0 ]; then
-    echo "sqlite3 patch has failed to apply with error code $?"
+    send_update $telegram_key $telegram_chat_id "sqlite3 patch has failed to apply with error code $?"
     exit 4
 fi
 
@@ -75,9 +121,9 @@ fi
 num_books=`sqlite3 flibusta_new.db "select count(1) from libbook"`
 
 if [ $num_books -le 500000 ]; then 
-  echo "Too few books, abort, abort!"
+  send_update $telegram_key $telegram_chat_id "Too few books ($num_books), abort, abort!"
 else
-  echo "New data dump looks legitimate, creating $flibusta_db_path"
+  send_update $telegram_key $telegram_chat_id "New data dump looks ($num_books books) legitimate, creating $flibusta_db_path"
   mv flibusta_new.db $flibusta_db_path
   rm lib*.sql
 fi
