e9a4ce4a8a8dcd4113ce4d300675d1193fe82737
Roman Kirillov <sigizmund@gmail.com>
2021-11-18 12:59:00 +0000

    Opening SQLite connection on server startup

--- data/flibuserver/server/flibustier_server.go
+++ data/flibuserver/server/flibustier_server.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	pb "flibustaimporter/flibuserver/proto"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"google.golang.org/grpc"
 )
 
 type server struct {
 	pb.UnimplementedFlibustierServer
+	Database *sql.DB
 }
 
 var (
-	port = flag.Int("port", 9000, "RPC server port")
+	port        = flag.Int("port", 9000, "RPC server port")
+	flibusta_db = flag.String("flibusta_db", "", "Path to Flibusta SQLite3 database")
 )
 
 func (s *server) GlobalSearch(ctx context.Context, in *pb.SearchRequest) (*pb.SearchResponse, error) {
@@ -25,6 +31,23 @@ func (s *server) GlobalSearch(ctx context.Context, in *pb.SearchRequest) (*pb.Se
 	return &pb.SearchResponse{
 		Result: "Hello " + in.GetSearchTerm(),
 	}, nil
+}
+
+func (s *server) Close() {
+	log.Println("Closing database connection.")
+	s.Database.Close()
+}
+
+func NewServer() (*server, error) {
+	srv := new(server)
+
+	db, err := sql.Open("sqlite3", *flibusta_db)
+	if err != nil {
+		return nil, err
+	}
+	srv.Database = db
+
+	return srv, nil
 }
 
 func main() {
@@ -35,7 +58,14 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterFlibustierServer(s, &server{})
+	srv, err := NewServer()
+	if err != nil {
+		log.Fatalf("Couldn't create server: %v", err)
+		os.Exit(2)
+	}
+	defer srv.Close()
+
+	pb.RegisterFlibustierServer(s, srv)
 	log.Printf("server listening at %v", lis.Addr())
 
 	if err := s.Serve(lis); err != nil {
