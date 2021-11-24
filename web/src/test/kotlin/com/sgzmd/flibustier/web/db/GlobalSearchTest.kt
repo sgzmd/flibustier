4540ec0a7dfe2ffec6bf5c276e19d7fe8fddf743
sgzmd <sigizmund@gmail.com>
2021-11-24 15:30:42 +0000

    Making workflow run

--- web/src/test/kotlin/com/sgzmd/flibustier/web/db/GlobalSearchTest.kt
+++ web/src/test/kotlin/com/sgzmd/flibustier/web/db/GlobalSearchTest.kt
@@ -10,8 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
 
 @SpringBootTest
 @RunWith(SpringRunner::class)
