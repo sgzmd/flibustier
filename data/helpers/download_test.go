8e43683cf6b20924626f71d3c51087ef003fd4df
sgzmd <sigizmund@gmail.com>
2021-11-28 17:26:01 +0000

    Fixing downloader test to use auto-assigned port rather than fail whenever 8080 is taken.

--- data/helpers/download_test.go
+++ data/helpers/download_test.go
@@ -11,23 +11,32 @@ import (
 
 const FILE_NAME = "local-file"
 
-type Server struct{}
+type Server struct {
+	Port int
+}
+
+var (
+	server *Server
+)
 
 func (server *Server) Start() {
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "result")
 	})
 	httpServer := &http.Server{}
-	listener, err := net.ListenTCP("tcp4", &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 8080})
+	listener, err := net.ListenTCP("tcp4", &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1)})
+
 	if err != nil {
 		log.Fatal("error creating listener")
 	}
+
+	server.Port = listener.Addr().(*net.TCPAddr).Port
 
 	go httpServer.Serve(listener)
 }
 
 func TestDownloadFile(t *testing.T) {
-	e := DownloadFile(FILE_NAME, "http://localhost:8080/hi")
+	e := DownloadFile(FILE_NAME, fmt.Sprintf("http://localhost:%d/hi", server.Port))
 	if e != nil {
 		t.Fatalf("Failed because of %s", e)
 	}
@@ -42,7 +51,7 @@ func TestDownloadFile(t *testing.T) {
 }
 
 func TestMain(m *testing.M) {
-	server := &Server{}
+	server = &Server{}
 	server.Start()
 
 	ret := m.Run()
